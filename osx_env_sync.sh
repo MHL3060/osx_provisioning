7064e5fefff46a6af20099cfe31e9f1aba026a63
MHL3060 <benli3060@gmail.com>
2018-03-21 12:00:40 -0700

    Update osx_env_sync.sh
    check env instead of bash_profile file

--- osx_env_sync.sh
+++ osx_env_sync.sh
@@ -1,5 +1,6 @@
-grep "^export" $HOME/.bash_profile | while IFS=' =' read ignoreexport envvar ignorevalue; do
-    echo "${envvar} ${!envvar}"
+#grep "^export" $HOME/.bash_profile | while IFS=' =' read ignoreexport envvar ignorevalue; do
+env | while IFS=' =' read envvar ignorevalue; do
+  echo "${envvar} ${!envvar}"
     if [ x$envvar != 'xPS1' ]; then
         launchctl setenv ${envvar} ${!envvar}
     fi
