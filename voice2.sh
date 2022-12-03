db0eaa77d8b7531039caa0185b11b781eb9df7d7
Ben Li <benli3060@gmail.com>
2022-12-03 12:31:25 -0800

    remove debug

--- voice2.sh
+++ voice2.sh
@@ -5,13 +5,12 @@ VOICE_COMMAND=""
 preexec() {
   VOICE_COMMAND=$1      
   START_TIME=$(date +%s)
-  echo "$START_TIME"
 }
 
 precmd() {
   END_TIME=$(date +%s)
   DURATION=$(($END_TIME - $START_TIME))
-  if [ $DURATION -gt 3 ] && [ "x${VOICE_COMMAND}" != "x" ]
+  if [ $DURATION -gt 10 ] && [ "x${VOICE_COMMAND}" != "x" ]
   then
           (say "command $VOICE_COMMAND done" &)
 	  unset VOICE_COMMAND
